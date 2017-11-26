d4d2d5b68d6a078ef39bcf509ff92d98758a1d34
Paul Hemmens <paulhemmens@live.nl>
2017-11-26 23:15:16 +0100

    first push

---
+++ README.md
@@ -0,0 +1,2 @@
+# HuisdierAssistentie
+# HuisdierAssistentie
---
+++ js/0.js
@@ -0,0 +1,14 @@
+if (location.protocol != 'https:')
+{
+ location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
+(adsbygoogle = window.adsbygoogle || []).push({
+google_ad_client: "ca-pub-1780312919233307",
+enable_page_level_ads: true
+});
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-104193585-1', 'auto');
+ga('send', 'pageview');
