15473fbcd606ad287f595f4e0657c10920b4e0bb
Paul Hemmens <paul.hemmens@sbs.nl>
2018-01-22 20:08:15 +0100

    again

--- duif.php
+++ duif.php
@@ -1,7 +1,7 @@
 <?php
 require 'PHPMailerAutoload.php';
 $mail = new PHPMailer;
-$mail->setFrom('from@example.com', 'Your Name');
+$mail->setFrom('info@huisdierassistentie.nl', 'Your Name');
 $mail->addAddress('myfriend@example.net', 'My Friend');
 $mail->Subject  = 'First PHPMailer Message';
 $mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
