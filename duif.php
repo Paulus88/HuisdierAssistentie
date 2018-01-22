65e28267de06cfb10f0f80509a8231358bc829d5
Paulus88 <paulhemmens@live.nl>
2018-01-22 21:46:32 +0100

    Add files via upload

--- duif.php
+++ duif.php
@@ -1,33 +1,32 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'vendor/autoload.php';
+require 'class.phpmailer.php';
 
-$mail = new PHPMailer(true);
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.zoho.eu';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'info@huisdierassistentie.nl';
-    $mail->Password = 'Tugba123';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
+$mail = new PHPMailer;
 
+$mail->isSMTP();
+$mail->Host = 'smtp.zoho.eu';                // Specify main and backup server
+$mail->Port = 587;                                    // Set the SMTP port
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->SMTPAuth = true;
+$mail->Username = 'info@huisdierassistentie.nl';
+$mail->Password = 'Tugba123';
+$mail->SMTPSecure = 'tls';                         // Enable encryption, 'ssl' also accepted
 
-    $mail->setFrom('info@huisdierassistentie.nl', 'Your Name');
-    $mail->addAddress('info@huisdierassistentie.nl', 'My Friend');
+$mail->From = 'info@huisdierassistentie.nl';
+$mail->FromName = 'Me';
+$mail->AddAddress('info@huisdierassistentie.nl');  // Add a recipient           // Name is optional
 
-    //Content
-    $mail->isHTML(true);
-    $mail->Subject = 'Test Mail Subject!';
-    $mail->Body    = 'This is SMTP Email Test';
+$mail->IsHTML(true);                                  // Set email format to HTML
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-}}
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <strong>in bold!</strong>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->Send()) {
+   echo 'Message could not be sent.';
+   echo 'Mailer Error: ' . $mail->ErrorInfo;
+   exit;
+}
+
+echo 'Message has been sent';
 ?>
