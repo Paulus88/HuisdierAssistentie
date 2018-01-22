b03b0dba98782827e4bd0014af7c3e3ae3f36f3e
Paulus88 <paulhemmens@live.nl>
2018-01-22 20:25:02 +0100

    Add files via upload

--- duif.php
+++ duif.php
@@ -1,14 +1,31 @@
-<?php
-require 'PHPMailerAutoload.php';
-$mail = new PHPMailer;
-$mail->setFrom('info@huisdierassistentie.nl', 'Your Name');
-$mail->addAddress('info@huisdierassistentie.nl', 'My Friend');
-$mail->Subject  = 'First PHPMailer Message';
-$mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
-if(!$mail->send()) {
-  echo 'Message was not sent.';
-  echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent.';
-}
-?>
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'username@gmail.com';
+    $mail->Password = '_password_';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+
+    $mail->setFrom('info@huisdierassistentie.nl', 'Your Name');
+    $mail->addAddress('info@huisdierassistentie.nl', 'My Friend');
+
+    //Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Test Mail Subject!';
+    $mail->Body    = 'This is SMTP Email Test';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}}
