752dba07aab6e6f239ddb0ad975b635dcbed71f1
Paulus88 <paulhemmens@live.nl>
2019-04-21 01:22:22 +0200

    Update duif.php

--- duif.php
+++ duif.php
@@ -2,7 +2,7 @@
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
     $from = "info@huisdierassistentie.nl";
-    $to = "YOUREMAILADDRESS";
+    $to = "info@huisdierassistentie.nl";
     $subject = "PHP Mail Test script";
     $message = "This is a test to check the PHP Mail functionality";
     $headers = "From:" . $from;
