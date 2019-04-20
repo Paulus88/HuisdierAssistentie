29804fc84231c76f08ad81fd6624ed5ff3260bee
Paulus88 <paulhemmens@live.nl>
2019-04-21 01:19:47 +0200

    Update duif.php

--- duif.php
+++ duif.php
@@ -6,6 +6,6 @@
     $subject = "PHP Mail Test script";
     $message = "This is a test to check the PHP Mail functionality";
     $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
+    mail($to,$subject,$message,$headers);
     echo "Test email sent";
 ?>
