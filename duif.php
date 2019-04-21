33deb017e55c310711e7069013e01e9ea1e8fd2e
Paulus88 <paulhemmens@live.nl>
2019-04-21 02:45:52 +0200

    Update duif.php

--- duif.php
+++ duif.php
@@ -9,7 +9,7 @@ $ras = $_POST['ras'];
 $formcontent="Van: $name \n Email: $email \n Telefoon: $tel \n Naam huisdier: $huisdier \n Soort dier: $soort \n Ras dier: $ras \n Message: $message";
 $recipient = "info@huisdierassistentie.nl";
 $subject = "Website: Hello Huisdier Assistentie";
-$mailheader = "From: $email \r\n";
+$mailheader = "From: $email" . "\r\n" . "Reply-To: $email" . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
