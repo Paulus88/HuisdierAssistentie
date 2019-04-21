0bb4e49ed7c1573a3100b7485b76f55334996b90
Paulus88 <paulhemmens@live.nl>
2019-04-21 03:23:26 +0200

    contact

--- duif.php
+++ duif.php
@@ -11,5 +11,16 @@ $recipient = "info@huisdierassistentie.nl";
 $subject = "Website: Hello Huisdier Assistentie";
 $mailheader = "From:" . $email;
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
 ?>
+<!doctype html>
+<html>
+<head>
+		<title>Huisdier Assistentie - Duif</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+<body>
+<video autoplay muted loop class="duif">
+  <source src="images/duif.mp4" type="video/mp4">
+</video>
+</body>
+</html>
