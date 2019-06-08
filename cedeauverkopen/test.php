e22a7bcc134305adfa98c14d6a18c399546671ec
Paulus88 <paulhemmens@live.nl>
2019-06-09 01:38:47 +0200

    Update test.php

--- cedeauverkopen/test.php
+++ cedeauverkopen/test.php
@@ -1,4 +1,17 @@
 <?php
     $useragent = $_SERVER['HTTP_USER_AGENT'];
     echo "<b>Your User Agent is</b>: ".$useragent;
+    $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+    return $ip;
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //check ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //to check ip is pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
 ?>
