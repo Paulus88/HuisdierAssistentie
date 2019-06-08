fd68eaae32eede4be77a4f87dcbfdabfc980cc6f
Paulus88 <paulhemmens@live.nl>
2019-06-09 00:25:36 +0200

    Update test.php

--- cedeauverkopen/test.php
+++ cedeauverkopen/test.php
@@ -1,6 +1,23 @@
 <?php
 include 'huMf6GwI0pbSg1k.php';
 $conn = OpenCon();
-echo "Connected Successfully";
+$result = mysqli_query($connection,"SELECT * FROM *");
+$all_property = array();  //declare an array for saving property
+echo '<table class="data-table">
+        <tr class="data-heading">';  //initialize table tag
+while ($property = mysqli_fetch_field($result)) {
+    echo '<td>' . $property->name . '</td>';  //get field name for header
+    array_push($all_property, $property->name);  //save those to array
+}
+echo '</tr>'; //end tr tag
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    foreach ($all_property as $item) {
+        echo '<td>' . $row[$item] . '</td>'; //get items using property value
+    }
+    echo '</tr>';
+}
+echo "</table>";
+?>
 CloseCon($conn);
 ?>
