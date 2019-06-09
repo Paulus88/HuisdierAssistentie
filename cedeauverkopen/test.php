eff69c22fd85fb53ffff3c8d29fa69d11c67afa8
Paulus88 <paulhemmens@live.nl>
2019-06-09 19:58:38 +0200

    Update test.php

--- cedeauverkopen/test.php
+++ cedeauverkopen/test.php
@@ -1,4 +1,11 @@
-<script>navigator.geolocation.getCurrentPosition(function(position){var lat = position.coords.latitude;var lon = position.coords.longitude;document.cookie = "lat=" + lat;document.cookie = "lon=" + lon;});</script>
+<script>
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+  document.cookie = "lat=" + lat;
+  document.cookie = "lon=" + lon;});
+</script>
 <?php
 $lat = $_COOKIE['lat'];
 echo $lat."<br />";
