4b8185d98741f92fb6d45a1f45cf7516c0d7a72b
Paulus88 <paulhemmens@live.nl>
2019-06-09 18:09:30 +0200

    Update test.php

--- cedeauverkopen/test.php
+++ cedeauverkopen/test.php
@@ -3,7 +3,7 @@ navigator.geolocation.getCurrentPosition(function(position){
 var lat = position.coords.latitude;
 var lon = position.coords.longitude;
 document.cookie="geo=" + lat + lon;
-document.getElementById("result").innerHTML = lat + lon;});
+document.getElementById("result").innerHTML = lat + ", " + lon;});
 </script><?php
 $geo = $_COOKIE['geo'];
 echo $geo."<br />";
