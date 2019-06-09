7308e72575cdbd05376ec86de05da42ee29f28a1
Paulus88 <paulhemmens@live.nl>
2019-06-09 11:18:05 +0200

    a

--- cedeauverkopen/test.php
+++ cedeauverkopen/test.php
@@ -1,18 +1,25 @@
 <?php
     $useragent = $_SERVER['HTTP_USER_AGENT'];
     echo $useragent."<br />";
-    $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
-    echo $ip."<br />";
-    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-    }else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    }else{
-        $ip = $_SERVER['REMOTE_ADDR'];
-    }
-    echo $ip."<br />";
-    $json_data = array(
-       'id'=>1,'name'=>'jackson','country'=>'usa','office'=>array('google','oracle')
-    );
-    echo json_encode($json_data);
+    function get_client_ip() {
+    if (getenv('HTTP_CLIENT_IP'))
+        $ip = getenv('HTTP_CLIENT_IP');
+        echo $ip
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        echo $ip
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+        echo $ip
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        echo $ip
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+       echo $ip
+    else(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+        echo $ip
+    return $ipaddress;}
+    echo $get_client_ip;
 ?>
