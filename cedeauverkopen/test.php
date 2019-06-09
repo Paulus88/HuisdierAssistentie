503d89cb3adfce82357b8e78922d21fb1183ea02
Paulus88 <paulhemmens@live.nl>
2019-06-09 14:49:19 +0200

    Update test.php

--- cedeauverkopen/test.php
+++ cedeauverkopen/test.php
@@ -1,11 +1,13 @@
 <script>
 navigator.geolocation.getCurrentPosition(function(position){
 var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
-var javavar=document.getElementById("result").innerHTML = positionInfo;
+var javavar=document.getElementById("text").value;
+document.getElementById("result").innerHTML = positionInfo;});
+document.getElementById("result").innerHTML="
 <?php
 $phpvar='"+javavar+"';
 echo $phpvar;
-?>";});
+?>";
 </script><?php
 $client = $_SERVER['HTTP_CLIENT_IP'];
 echo $client."<br />";
