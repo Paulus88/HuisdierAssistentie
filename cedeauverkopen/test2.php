b0e5ff3568bc8378d7148f6da5c3cb05a6e95266
Paulus88 <paulhemmens@live.nl>
2019-06-10 15:26:11 +0200

    Update test2.php

--- cedeauverkopen/test2.php
+++ cedeauverkopen/test2.php
@@ -1,9 +1,11 @@
-<?php $conn = OpenCon();
+<?php
+include 'huMf6GwI0pbSg1k.php';
+$conn = OpenCon();
     if ($conn->connect_errno) {
         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
     $sql = "SELECT * FROM track;";  //edit your table name here
     $res = $mysqli->query($sql);
-
     while ($row = $res->fetch_assoc()) {
-    print_r($row);} ?>
+    print_r($row);}
+    CloseCon($conn); ?>
