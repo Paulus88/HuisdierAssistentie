5492cdc1dc5ab3020b47f385779530a9600f7869
Paulus88 <paulhemmens@live.nl>
2019-06-10 15:38:53 +0200

    Update test2.php

--- cedeauverkopen/test2.php
+++ cedeauverkopen/test2.php
@@ -5,7 +5,6 @@ if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
-echo $conn;
 $result = $conn->query("SELECT * FROM track");
   echo $result;
     printf("Select returned %d rows.\n", $result->num_rows);
