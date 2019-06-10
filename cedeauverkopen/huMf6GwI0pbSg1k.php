d0094af374d27583d34bed8b667346a0b9c62984
Paulus88 <paulhemmens@live.nl>
2019-06-11 01:09:28 +0200

    a

--- cedeauverkopen/huMf6GwI0pbSg1k.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "huisdierassistentie.cacxu1b4dzkf.eu-central-1.rds.amazonaws.com";
-$user = "logger";
-$pass = "1Logger21!";
-$db = "cedeauverkopen";
-$conn = new mysqli($host, $user, $pass, $db);
-?>
