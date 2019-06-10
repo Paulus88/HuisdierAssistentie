2dbcdb74f877bb17eea4b726c47c702e150e2a01
Paulus88 <paulhemmens@live.nl>
2019-06-10 18:21:46 +0200

    Update test2.php

--- cedeauverkopen/test2.php
+++ cedeauverkopen/test2.php
@@ -1,6 +1,15 @@
 <?php
 include 'huMf6GwI0pbSg1k.php';
-$query = "SELECT * FROM track";
-$result = mysqli_query($conn, $query);
-echo $result;
-$conn->close(); ?>
+
+$query = "SELECT * FROM track"; //You don't need a ; like you do in SQL
+$result = mysqli_query($query);
+
+echo "<table>"; // start a table tag in the HTML
+
+while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
+echo "<tr><td>" . $row['name'] . "</td><td>" . $row['age'] . "</td></tr>";  //$row['index'] the index here is a field name
+}
+
+echo "</table>"; //Close the table in HTML
+
+mysql_close(); //Make sure to close out the database connection ?>
