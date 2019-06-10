f63a9858db73ab937d254933d0002314b91f2686
Paulus88 <paulhemmens@live.nl>
2019-06-10 21:39:10 +0200

    Update test2.php

--- cedeauverkopen/test2.php
+++ cedeauverkopen/test2.php
@@ -5,9 +5,9 @@ $sql="SELECT * FROM track ORDER BY id DESC";
 if ($result=mysqli_query($conn,$sql))
   {
   // Fetch one and one row
-  while ($row=mysqli_fetch_row($result))
+  while($row=mysqli_fetch_row($result))
     {
-    printf ("<b>Time: %s<br /><br />IPs: %s | %s | %s | %s | %s | %s | %s<br /><br />Location: lat %s | lon %s</b><br /><a href=%s target=_blank>Google Maps</a><br />%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s<br />",$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],$row[7],$row[8],$row[9],$row[10],$row[11],$row[12],$row[13],$row[
+    printf("<b>Time: %s<br /><br />IPv4s: %s | %s | %s | %s | %s | %s | %s<br /><br />Location: lat %s | lon %s</b><br /><a href=%s target=_blank>Google Maps</a><br />IPv6s: %s | %s | %s | %s | %s | %s | %s<br /><br />Agent OS/Browser: %s %s %s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />%s<br /><br />",$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],$row[7],$row[8],$row[9],$row[10],$row[11],$row[12],$row[13],$row[
   14],$row[15],$row[16],$row[17],$row[18],$row[19],$row[20],$row[21],$row[22],$row[23],$row[24],$row[25],$row[26],$row[27],$row[28],$row[29],$row[30],$row[31],$row[32],$row[33],$row[34],$row[35]);
     }
   mysqli_free_result($result);
