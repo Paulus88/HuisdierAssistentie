25d230524c32a7448216565c6d91b2a22ce25a87
Paulus88 <paulhemmens@live.nl>
2019-06-10 19:17:22 +0200

    a

--- cedeauverkopen/huMf6GwI0pbSg1k.php
+++ cedeauverkopen/huMf6GwI0pbSg1k.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "huisdierassistentie.cacxu1b4dzkf.eu-central-1.rds.amazonaws.com";
-$username = "logger";
-$password = "1Logger21!";
-$dbname = "cedeauverkopen";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$host = "huisdierassistentie.cacxu1b4dzkf.eu-central-1.rds.amazonaws.com";
+$user = "logger";
+$pass = "1Logger21!";
+$db = "cedeauverkopen";
+$conn = new mysqli($host, $user, $pass, $db);
 ?>
--- cedeauverkopen/test2.php
+++ cedeauverkopen/test2.php
@@ -1,10 +1,7 @@
 <?php
 include 'huMf6GwI0pbSg1k.php';
-$result = mysqli_query("select * from track");
-while ($row = mysql_fetch_object($result)) {
-    echo $row->id;
-    echo $row->ip;
+foreach ( $db->query('SELECT * FROM track') as $row ) {
+    print_r($row);//echo "{$row['field']}";
 }
-mysql_free_result($result);
 mysqli_close($conn);
 ?>
