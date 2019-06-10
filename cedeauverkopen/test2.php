494c36ce8c37800e02174c05e2b7c94fff013426
Paulus88 <paulhemmens@live.nl>
2019-06-10 15:29:11 +0200

    Update test2.php

--- cedeauverkopen/test2.php
+++ cedeauverkopen/test2.php
@@ -1,6 +1,10 @@
 <?php
 include 'huMf6GwI0pbSg1k.php';
 $conn = OpenCon();
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
     $sql = "SELECT * FROM track;";  //edit your table name here
     $res = $mysqli->query($sql);
     while ($row = $res->fetch_assoc()) {
