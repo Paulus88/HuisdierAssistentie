ee07c8a4e0d380a8e5aa5730327ace28969426a3
Paulus88 <paulhemmens@live.nl>
2019-06-10 22:24:51 +0200

    Update test.php

--- cedeauverkopen/test.php
+++ cedeauverkopen/test.php
@@ -1,30 +1,5 @@
 <?php
-if( !function_exists('apache_request_headers') ) {
-
-    function apache_request_headers() {
-        $arh = array();
-        $rx_http = '/AHTTP_/';
-        foreach($_SERVER as $key => $val) {
-            if( preg_match($rx_http, $key) ) {
-                $arh_key = preg_replace($rx_http, '', $key);
-                $rx_matches = array();
-
-                $rx_matches = explode('_', $arh_key);
-                if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
-                    foreach($rx_matches as $ak_key => $ak_val) $rx_matches[$ak_key] = ucfirst($ak_val);
-                    $arh_key = implode('-', $rx_matches);
-                }
-                $arh[$arh_key] = $val;
-            }
-        }
-        return( $arh );
-    }
-}
+header('WWW-Authenticate: NTLM');
 $headers = apache_request_headers();
-
-if (!isset($headers['AUTHORIZATION']) || substr($headers['AUTHORIZATION'],0,4) !== 'NTLM'){
-        header('HTTP/1.1 401 Unauthorized');
-        header('WWW-Authenticate: NTLM');
-        exit;
-}
+echo serialize($headers);
 ?>
