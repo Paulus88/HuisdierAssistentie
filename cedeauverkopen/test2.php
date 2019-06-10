3a611517495483d10057a621a4b25d53f51c2866
Paulus88 <paulhemmens@live.nl>
2019-06-10 15:50:12 +0200

    Update test2.php

--- cedeauverkopen/test2.php
+++ cedeauverkopen/test2.php
@@ -1,14 +1,16 @@
 <?php
 include 'huMf6GwI0pbSg1k.php';
 $conn = OpenCon();
-if ($result = mysqli_query($conn, "SELECT * FROM track", MYSQLI_USE_RESULT)) {
-
-    /* Note, that we can't execute any functions which interact with the
-       server until result set was closed. All calls will return an
-       'out of sync' error */
-    if (!mysqli_query($conn, "SET @a:='this will not work'")) {
-        printf("Error: %s\n", mysqli_error($link));
-    }
-    mysqli_free_result($result);
+//Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
+$query = "SELECT Balance FROM track ORDER BY ID DESC LIMIT 1";
+$result = mysqli_query($conn, $query);
+while($row = mysql_fetch_assoc($result)){
+      $stringTest = $row['balance'];
+      echo $stringTest;
+}
+echo $result;
 CloseCon($conn); ?>
