46e5da390e77a274ad33c2543a89ae360e0569fb
Paulus88 <paulhemmens@live.nl>
2019-06-10 17:01:19 +0200

    Update test2.php

--- cedeauverkopen/test2.php
+++ cedeauverkopen/test2.php
@@ -1,6 +1,6 @@
 <?php
 include 'huMf6GwI0pbSg1k.php';
-$query = "SELECT * FROM track ORDER BY ID DESC";
+$query = "SELECT * FROM track";
 $result = mysqli_query($conn, $query);
 echo $result;
 $conn->close(); ?>
