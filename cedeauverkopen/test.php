d3e781714bd0b5c934d0fb34d2ac1570fd9d9749
Paulus88 <paulhemmens@live.nl>
2019-06-10 13:29:27 +0200

    Update test.php

--- cedeauverkopen/test.php
+++ cedeauverkopen/test.php
@@ -95,5 +95,11 @@ if(mysqli_query($conn, $sql)){
 }else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
+$sql = "INSERT INTO track(client) VALUES ('$client')";
+if(mysqli_query($conn, $sql)){
+    echo "Records added successfully.";
+}else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
 CloseCon($conn);
 ?>
