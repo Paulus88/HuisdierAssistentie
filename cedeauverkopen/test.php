0f368b9be04334de15e140e85ff89ee5a851b388
Paulus88 <paulhemmens@live.nl>
2019-06-10 16:36:05 +0200

    Update test.php

--- cedeauverkopen/test.php
+++ cedeauverkopen/test.php
@@ -88,8 +88,8 @@ $ipageo = serialize(file_get_contents("http://geoplugin.net/php.gp?ip=".$ipa));
 if ($ipav6 != 0){
 $ipav6geo = serialize(file_get_contents("http://geoplugin.net/php.gp?ip=".$ipav6));}}
 $servername = "huisdierassistentie.cacxu1b4dzkf.eu-central-1.rds.amazonaws.com";
-$username = "ha";
-$password = "CandyKitty16!";
+$username = "logger";
+$password = "1Logger21";
 $dbname = "cedeauverkopen";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
