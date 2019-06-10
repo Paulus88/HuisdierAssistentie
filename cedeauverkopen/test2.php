5af050ef6df2de3c4b6e8f4dc263c1b2e73c9bb8
Paulus88 <paulhemmens@live.nl>
2019-06-10 20:29:11 +0200

    Update test2.php

--- cedeauverkopen/test2.php
+++ cedeauverkopen/test2.php
@@ -1,7 +1,17 @@
 <?php
 include 'huMf6GwI0pbSg1k.php';
-foreach ( $db->query('SELECT * FROM track') as $row ) {
-    print_r($row);
+$sql="SELECT * FROM track ORDER BY id";
+
+if ($result=mysqli_query($conn,$sql))
+  {
+  // Fetch one and one row
+  while ($row=mysqli_fetch_row($result))
+    {
+    printf ("%s (%s)\n",$row[0],$row[1]);
+    }
+  // Free result set
+  mysqli_free_result($result);
 }
+
 mysqli_close($conn);
 ?>
