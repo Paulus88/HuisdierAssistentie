e41430e9626e524648bf0d346d7f62b5c028de5f
Paulus88 <paulhemmens@live.nl>
2019-06-10 16:06:25 +0200

    Update sql.php

--- cedeauverkopen/sql.php
+++ cedeauverkopen/sql.php
@@ -1,9 +1,8 @@
 <?php
-
    $dbhost = 'huisdierassistentie.cacxu1b4dzkf.eu-central-1.rds.amazonaws.com';
    $dbuser = 'logger';
    $dbpass = '1Logger21';
-   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   $conn = mysqli($dbhost, $dbuser, $dbpass);
 
    if(! $conn ) {
       die('Could not connect: ' . mysql_error());
