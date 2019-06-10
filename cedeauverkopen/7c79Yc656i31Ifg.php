f50717dbbd9e727a31a66ece4ef61f7663c5e23c
Paulus88 <paulhemmens@live.nl>
2019-06-11 00:53:11 +0200

    a

--- cedeauverkopen/7c79Yc656i31Ifg.php
+++ cedeauverkopen/7c79Yc656i31Ifg.php
@@ -32,5 +32,8 @@ if ($unicode)
 return str_replace("\0", '', substr($msg, $off, $len));
 else
 return substr($msg, $off, $len);
-}}}
+}
+$user = get_msg_str($msg, 36);
+$domain = get_msg_str($msg, 28);
+$host = get_msg_str($msg, 44);}}
 ?>
